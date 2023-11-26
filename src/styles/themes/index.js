83cb0e397235c8c20d6fe269c3275e47a09987a9
Pablo Gabriel <79858234+Poveii@users.noreply.github.com>
2023-11-26 17:43:36 +0000

    feat: Paleta de cores e temas

---
+++ src/styles/themes/index.js
@@ -0,0 +1,24 @@
+export const themes = {
+  dark: {
+    primary: '#f23041',
+    secondary: '#3f0440',
+    tertiary: '#bf2642',
+    background: '#3b3a39',
+    text: '#f2f2f0',
+    border: {
+      initial: '#242629',
+      final: '#141414',
+    },
+  },
+  light: {
+    primary: '#f23041',
+    secondary: '#3f0440',
+    tertiary: '#bf2642',
+    background: '#ecf0f3',
+    text: '#181818',
+    border: {
+      initial: '#d1d9e6',
+      final: '#f9f9f9',
+    },
+  },
+}
